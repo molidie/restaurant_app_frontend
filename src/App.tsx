a5a7c27a1f94ee2aba1625325e09be148cc63631
molidie <molinerodiego5@gmail.com>
2024-05-21 03:00:43 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from './components/navbar.tsx';
+import Image from './components/image.tsx';
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Image />
+    </>
+  )
+}
+export default App
